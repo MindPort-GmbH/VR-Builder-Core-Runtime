eb8fade1e859a9ae041949f32abf270a9a06f602
Zaid Wajid <xaidwajid@gmail.com>
2026-06-12 19:19:32 +0200

    Update LifeCycle.cs

--- Source/LifeCycle.cs
+++ Source/LifeCycle.cs
@@ -312,7 +312,6 @@ namespace VRBuilder.Core
         {
             string path = EntityPathUtils.BuildRichTextEntityPath(Owner);
             Debug.LogError($"Exception at {path} while <b>{Stage} ({function})</b>\n{exception}");
-            VRBConsole.LogError($"Exception at {path} while <b>{Stage} ({function})</b>\n{exception}");
         }
     }
 }
